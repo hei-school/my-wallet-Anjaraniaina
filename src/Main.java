c08c741ddc52ea7f29f74b0ba57b975aba02a8e5
Anjaraniaina <hei.anjaraniaina@gmail.com>
2023-12-07 15:15:34 +0300

    feat: currency convert

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,29 @@
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    static final Scanner scanner = new Scanner(System.in);
+    static final Service service = new Service(0, new ArrayList<>(), "Ariary");
+
+
     public static void main(String[] args) {
+        welcome();
         menu();
     }
+    public static double doubleScanner() {
+        return scanner.nextDouble();
+    }
 
-    public static int scanner() {
-        Scanner scanner = new Scanner(System.in);
+    public static int intScanner() {
         return scanner.nextInt();
     }
 
-    public static void menu() {
+    public static void welcome(){
         Service.print("Hello! I'm your smart wallet\n" + "what do you want to do ?");
+    }
+
+    public static void menu() {
         Service.print(new StringBuilder()
                 .append("1. Check your current balance.")
                 .append("\n")
@@ -20,21 +31,20 @@ public class Main {
                 .append("\n")
                 .append("3. Store money.")
                 .append("\n")
-                .append("4. Epargne your money.")
+                .append("4. Convert to another currency.")
                 .append("\n")
                 .append("5. Check your 5 latest transactions.")
                 .append("\n")
                 .append("6. Quit.")
                 .toString());
-        Service service = new Service();
-        switch (scanner()){
-            case 1: service.checkBalance();
-                break;
-            case 2: service.retrieveMoney();
-            case 3: service.storeMoney();
-            case 4: service.epargneMoney();
-            case 5: service.history();
-            case 6: quit();
+        switch (intScanner()) {
+            case 1 -> service.checkBalance();
+            case 2 -> service.retrieveMoney();
+            case 3 -> service.storeMoney();
+            case 4 -> service.convertCurrency();
+            case 5 -> service.history();
+            case 6 -> quit();
+            default -> menu();
         }
     }
 
